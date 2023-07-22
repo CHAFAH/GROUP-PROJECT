817cf6d8ff8986fb7c8165d944ca35118990bf07
Prince Chafah Forchu Sani <125821852+CHAFAH@users.noreply.github.com>
2023-07-22 10:37:03 +0200

    terraformFiles

---
+++ terraform/vars.tf
@@ -0,0 +1,18 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+variable "ZONE2" {
+  default = "us-east-1b"
+}
+
+variable "AMI" {
+  default = "ami-026ebd4cfe2c043b2"
+}
+variable "instance_type" {
+  default = "t2-micro"
+}
