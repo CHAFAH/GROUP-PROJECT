591efb940bd2cab362c2bfcf5030cd36f79ea084
Prince Chafah Forchu Sani <125821852+CHAFAH@users.noreply.github.com>
2023-07-31 23:59:49 +0200

    Update cluster.tf

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -50,3 +50,19 @@ resource "aws_eks_addon" "ebs-csi" {
     "terraform" = "true"
   }
 }
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+
+output "region" {
+  description = "AWS region"
+  value       = var.REGION
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
