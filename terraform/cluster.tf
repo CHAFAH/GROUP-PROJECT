518e38d871a283741ad7687293490914ad174d28
Prince Chafah Forchu Sani <125821852+CHAFAH@users.noreply.github.com>
2023-07-31 23:21:53 +0200

    Update cluster.tf

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -12,9 +12,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     general = {
-      desired_size = 1
+      desired_size = 2
       min_size     = 1
-      max_size     = 10
+      max_size     = 4
 
       labels = {
         role = "general"
@@ -23,5 +23,30 @@ module "eks" {
       instance_types = ["t3.small"]
       capacity_type  = "ON_DEMAND"
     }
+  }
+}
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "4.7.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+resource "aws_eks_addon" "ebs-csi" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.20.0-eksbuild.1"
+  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+  tags = {
+    "eks_addon" = "ebs-csi"
+    "terraform" = "true"
   }
 }
